20e75417ad86d64616bd0cb8bce736d702690a5a
Muhammad Tayyab <tayyabsultan621@gmail.com>
2024-05-23 12:56:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const productRouter = require("./routes/products.js");
+const userRouter = require("./routes/users.js");
+
+const server = express();
+server.use(cors());
+server.use(express.urlencoded());
+server.use(express.static(path.resolve(__dirname, "public")));
+server.use(express.json());
+server.use("/products", productRouter.router);
+server.use("/users", userRouter.router);
+server.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://MuhammadTayyab:hXeg3v1P3m2vngbi@cluster0.pine65s.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("MongoDB Connected");
+    server.listen(3000, () => {
+      console.log("Server started");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
